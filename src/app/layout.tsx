f2745180fc24ecf40b429b32441aa4531cdb914b
Conroy Whitney <conroy.whitney@gmail.com>
2025-03-20 23:57:03 -0400

    Ignore hydration issues (safe?)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export const metadata: Metadata = {
   title: 'MLS Search Tool',
   description: 'Find properties based on drive time and budget',
@@ -16,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   )
 }
